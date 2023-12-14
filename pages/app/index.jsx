f521138bc2fe7627b03031e31460a2bd655483b3
s.hossein <s.hossein@rahkar.co>
2023-12-14 18:29:03 +0330

    routing for landing page app and aboutus

---
+++ pages/app/index.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>Application</h1>;
+};
+
+export default App;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,2 +1,24 @@
-import AboutUs from "./about-us";
-export default AboutUs;
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Link from "next/link";
+
+const LandingPage = () => {
+  return (
+    <Box sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+      <Grid
+        container
+        justifyContent={"center"}
+        alignItems={"center"}
+        sx={{ flex: 1 }}
+      >
+        <Link href="/app">
+          <Button variant="contained">Go to App</Button>
+        </Link>
+      </Grid>
+    </Box>
+  );
+};
+
+export default LandingPage;
