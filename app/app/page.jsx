2ca320090897d18b69767b70c17c0c8cf7619047
s.hossein <s.hossein@rahkar.co>
2023-12-21 12:42:58 +0330

    adding inital map to app

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,9 +1,40 @@
+"use client";
+import { useState, useMemo } from "react";
 import Grid from "@mui/material/Grid";
+import Map from "components/map/map";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const App = () => {
+  const [tabValue, setTabValue] = useState(0);
+  const handleChange = (e, value) => {
+    setTabValue(value);
+  };
+
+  const Content = useMemo(
+    () => ({
+      0: <Map />,
+      1: <></>,
+      2: <></>,
+    }),
+    []
+  );
+
   return (
-    <Grid>
-      <h1>{`Our App Will Be Here Soon. ;) `} </h1>
+    <Grid container justifyContent={"center"}>
+      <Tabs
+        value={tabValue}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+        full
+      >
+        <Tab label="Map" />
+        <Tab label="List" />
+        <Tab label="Filter" />
+      </Tabs>
+      <Grid container sx={{ mt: 3 }}>
+        {Content[tabValue]}
+      </Grid>
     </Grid>
   );
 };
---
+++ src/components/map/map.jsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "./map.css";
+import Grid from "@mui/material/Grid";
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(-123.1207);
+  const [lat] = useState(49.2827);
+  const [zoom] = useState(14);
+  const [API_KEY] = useState("2dqVQNThsS932KjZq6KS");
+
+  useEffect(() => {
+    if (map.current) return; // stops map from intializing more than once
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  }, [API_KEY, lng, lat, zoom]);
+
+  return (
+    <Grid container className="map-wrap">
+      <div ref={mapContainer} className="map" />
+    </Grid>
+  );
+}
